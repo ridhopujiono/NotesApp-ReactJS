7dd1d0191cabf51cfe6fc70076a45031e127a70a
Ridho Pujiono <68780041+ridhopujiono@users.noreply.github.com>
2022-10-08 10:20:52 +0700

    Membuat initial databuntuj event

--- src/App.js
+++ src/App.js
@@ -20,7 +20,50 @@ const App = () => {
     return newDate;
   }
 
- const [events, setEvents] = useState([]);
+ const [events, setEvents] = useState([
+   {
+    id: nanoid()+new Date().getTime(),
+    title: "Babel",
+    body: "Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.",
+    created_at: '2022-04-14T04:27:34.572Z',
+    archived: false,
+  },
+  {
+    id: nanoid()+new Date().getTime(),
+    title: "Functional Component",
+    body: "Functional component merupakan React component yang dibuat menggunakan fungsi JavaScript. Agar fungsi JavaScript dapat disebut component ia harus mengembalikan React element dan dipanggil layaknya React component.",
+    created_at: '2022-04-14T04:27:34.572Z',
+    archived: false,
+  },
+  {
+    id: nanoid()+new Date().getTime(),
+    title: "Modularization",
+    body: "Dalam konteks pemrograman JavaScript, modularization merupakan teknik dalam memecah atau menggunakan kode dalam berkas JavaScript secara terpisah berdasarkan tanggung jawabnya masing-masing.",
+    created_at: '2022-04-14T04:27:34.572Z',
+    archived: false,
+  },
+  {
+    id: nanoid()+new Date().getTime(),
+    title: "Lifecycle",
+    body: "Dalam konteks React component, lifecycle merupakan kumpulan method yang menjadi siklus hidup mulai dari component dibuat (constructor), dicetak (render), pasca-cetak (componentDidMount), dan sebagainya. ",
+    created_at: '2022-04-14T04:27:34.572Z',
+    archived: false,
+  },
+  {
+    id: nanoid()+new Date().getTime(),
+    title: "ESM",
+    body: "ESM (ECMAScript Module) merupakan format modularisasi standar JavaScript.",
+    created_at: '2022-04-14T04:27:34.572Z',
+    archived: false,
+  },
+  {
+    id: nanoid()+new Date().getTime(),
+    title: "Module Bundler",
+    body: "Dalam konteks pemrograman JavaScript, module bundler merupakan tools yang digunakan untuk menggabungkan seluruh modul JavaScript yang digunakan oleh aplikasi menjadi satu berkas.",
+    created_at: '2022-04-14T04:27:34.572Z',
+    archived: false,
+  },
+ ]);
 
 const addEventHandler = (title, body) =>{
   const date = new Date();
